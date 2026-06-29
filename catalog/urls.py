877d311d52bbbc9d552ec8d5af73b526c073b577
VadymVolynko <world.ow1@ukr.net>
2026-06-29 10:39:30 +0300

    fix: update catalog urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
-from catalog.views import home_view
+from catalog.views import home_view, catalog_view, product_view
 
 urlpatterns = [
     path("", home_view, name="home"),
+    path("catalog/", catalog_view, name="catalog"),
+    path("catalog/<slug:slug>/", product_view, name="product"),
 ]
