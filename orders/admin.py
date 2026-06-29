8117c45b6f820829ee5dc9c7f0db0359b0b6e61c
VadymVolynko <world.ow1@ukr.net>
2026-06-29 10:35:29 +0300

    feat: add admin configuration for orders

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from orders.models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    readonly_fields = ("name", "price", "quantity")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display  = ("id", "first_name", "last_name", "email", "status", "total_price", "created_at")
+    list_filter   = ("status",)
+    search_fields = ("first_name", "last_name", "email")
+    list_editable = ("status",)
+    inlines       = [OrderItemInline]
